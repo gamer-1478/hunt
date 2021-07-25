819c10b2994ede287c200cd077c4cf078431f414
gamer-12748 <aayushgarg2006@gmail.com>
2021-07-25 18:10:46 +0530

    changes

--- index.js
+++ index.js
@@ -8,9 +8,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+var fs = require('fs');
+
+
+async function basicChecks(req) {
+    //takes in the whole req and gives true or error string, if true continue, or else return the error string given by this function.
+    let rawdata = fs.readFileSync('data.json');
+    let data = JSON.parse(rawdata);
+
+    let ip = req.headers["x-forwarded-for"];
+    if (ip) {
+        var list = ip.split(",");
+        ip = list[list.length - 1];
+    } else {
+        ip = req.socket.remoteAddress;
+    }
+    //console.log("headers:", req.headers)
+    console.log("query:", req.query)
+    console.log("body:", req.body)
+    console.log(ip)
+
+    newIpField = { "ip_field": ip }
+    data.push(newIpField)
+    fs.writeFile('data.json', JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
+}
+
 //api for coockie changing ctf.
 app.get('/api/hddn', (req, res) => {
-    console.log(req.query)
+    basicChecks(req)
     let secret = req.query.secret || null;
     if (secret == "newshepard") {
         res.send({
@@ -23,14 +51,15 @@ app.get('/api/hddn', (req, res) => {
     }
 })
 
-app.get('/dino', function(req, res) {
+app.get('/dino', function (req, res) {
+    basicChecks(req)
     res.sendFile(path.join(__dirname, '/dino.html'));
-  });
-  
-  
+});
+
 //ejs file for cookie changing ctf.
 app.get('*', async (req, res) => {
+    basicChecks(req)
     res.render('index', { title: 'ChepBin' })
 })
 
-app.listen(port, () => console.log(`Server is running on port ${port} likely on localhost, if not on localhost then you are in production but you already know that!`));
+app.listen(port, () => console.log(`Server is running on port ${port} likely on localhost on your custom domain in production`));
